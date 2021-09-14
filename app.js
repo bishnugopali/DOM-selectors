dd5fa74bc49cef72dc556cf7539f27c2c38d57ba
bishnugopali <bishnugopali@gmail.com>
2021-09-14 07:31:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const headline=document.querySelector('#headline');
+const btnUpdateHeading=document.querySelector('.btn-main');
+
+const highlights=document.querySelectorAll('.highlight');
+
+const items=document.querySelectorAll('li');
+
+
+//for...of loop: https://www.digitalocean.com/community/tutorials/for-loops-for-of-loops-and-for-in-loops-in-javascript
+for(const highlight of highlights){
+  highlight.style.backgroundColor='cornsilk';
+}
+
+for(let i=0; i<items.length; i++){
+      items[i].style.color='orchid';
+    }
+
+
+btnUpdateHeading.addEventListener('click', () =>{
+    headline.style.border="solid 2px red";
+    headline.style.fontSize='60px';
+                                  
+
+});
+
+//selecting element by id
